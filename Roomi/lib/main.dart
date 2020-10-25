3afc983367d938f97e6f99a249475802bb62e05c
Sawan Kag <sawan@localhost.localdomain>
2020-10-25 13:31:04 +0530

    login page add

--- Roomi/lib/main.dart
+++ Roomi/lib/main.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/material.dart';  
-
+import 'package:flutter/material.dart';
+import 'package:Roomi/src/welcomePage.dart';
 import 'splashscreen.dart';
-  
-void main() => runApp(MyApp());  
-  
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-    const MyApp({Key key}) : super(key: key);
-  @override  
-  Widget build(BuildContext context) {  
-    return MaterialApp(  
+  const MyApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreenPage(),
-    );  
-  }  
-}  
+      home: WelcomePage(),
+    );
+  }
+}
